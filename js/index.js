815110a46a7c74287000c633b4bd1d9eded01f34
shenlin <shenlin192@gmail.com>
2017-01-27 20:55:59 +0100

    thumbnail perfait

--- js/index.js
+++ js/index.js
@@ -1,31 +1,40 @@
 $(document).ready(function() {
-        $('.carousel-responsive').slick({
-            infinite: true,
-            speed: 700,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            arrows: false,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            mobileFirst: true,
-            responsive: [{
-                    breakpoint: 991,
-                    settings: {
-                        arrows: false,
-                        centerMode: true,
-                        centerPadding: '40px',
-                        slidesToShow: 2
-                    }
-                },
-                {
-                    breakpoint: 768,
-                    settings: {
-                        arrows: false,
-                        centerMode: true,
-                        centerPadding: '40px',
-                        slidesToShow: 1
-                    }
+    $('.carousel-responsive').slick({
+        // dots: true,
+        // infinite: false,
+        // speed: 300,
+        // slidesToShow: 4,
+        // slidesToScroll: 4,
+        infinite: true,
+        speed: 700,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        autoWidth: true,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    arrows: true,
+                    dots: true
                 }
-            ]
-        });
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                  arrows: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 });
--- read.md
+++ read.md
@@ -4,6 +4,7 @@
 ## Some improvements
 
 ### Home page navigation
+
 1. See the vertical cut-off lines in the navigation bar. I think it would be better to create them will `border-left` or `before`, `after` pseudo-elements. Creating an empty Dom element will slow down the page.
 
 2. Using media queries `min-width rather` then `max-width` (`#imgLogoHoolders`) may be better with respect to mobile first principle. Especially since Bootstrap is a mobile first framework.
@@ -14,6 +15,12 @@ the title will collapse into each other
 
 ## More
 // Todo :
+
 1 add alt for each image
 2 mobile first
+3 thumbnail back-end content can't completely display
+4 carousel arrow responsive positioning {use `transform:scale(2)` to change its size and left right to
+   position them}
+5 make good use of the bootstrap grid system 
+
 specific each device sizes -xm -sm -md -lg
